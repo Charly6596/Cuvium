a8d786e87a5786efd6c81175908153e892fbddee
Carlos Javier Gutiérrez Sánchez <cgutisanc@gmail.com>
2019-03-21 22:17:55 +0100

    Add base entities

---
+++ Assets/Cuvium/Entities/MapObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cuvium.Entities
+{
+    public abstract class MapObject : ScriptableObject
+    {
+        public Player Owner;
+        public float Health;
+        public string Name;
+    }
+}
+
---
+++ Assets/Items/Scripts/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    public string Name;
+    public string Description;
+    public Texture2D Icon;
+    public AudioClip UsageSound;
+}
+
