92a400f36be7a9f3662bac13ff4527dfd99d6b18
Carlos Javier Gutiérrez Sánchez <cgutisanc@gmail.com>
2019-03-25 18:37:07 +0100

    Implement ButtonAttribute

---
+++ Assets/Scripts/Attributes/Button.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Cuvium.Core
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ButtonAttribute : PropertyAttribute
+    {
+        private string _label;
+        public string Label 
+        {
+            get
+            {
+                return _label;
+            }
+            set
+            {
+                _label = value;
+                IsLabelDefined = true;
+            }
+        }
+        public bool IsLabelDefined;
+
+        public ButtonAttribute()
+        {
+        }
+    }
+}
+
